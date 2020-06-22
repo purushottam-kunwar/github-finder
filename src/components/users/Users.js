3235e664b315535ded1793a4b11d4bbe01289ec3
pk.azminds <pk.azminds@gmail.com>
2020-06-22 21:13:55 +0545

    Spinner Component and Refactoring

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import UserItems from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map((user) => (
+        {users.map((user) => (
           <UserItems key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
+
+Users.PropTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: "grid",
