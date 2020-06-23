a6467b42f1d46cb76cf805720801a6819c4a0f34
pk.azminds <pk.azminds@gmail.com>
2020-06-23 13:18:35 +0545

    basic react router setup

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const NavBar = ({ title, icon }) => {
   return (
@@ -11,6 +12,14 @@ const NavBar = ({ title, icon }) => {
           <FontAwesomeIcon icon={icon} />
           {title}
         </h1>
+        <ul>
+          <li>
+            <Link to="/"> Home </Link>
+          </li>
+          <li>
+            <Link to="/about"> About </Link>
+          </li>
+        </ul>
       </nav>
     </div>
   );
