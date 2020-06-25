0c8f991f85b8c8f4e315c2b5a0754341deb84e66
pk.azminds <pk.azminds@gmail.com>
2020-06-25 16:41:09 +0545

    create reducer and actions

--- src/App.js
+++ src/App.js
@@ -29,16 +29,6 @@ const App = () => {
   // }
 
   // search github users
-  const searchUsers = async (text) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&clint_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&clint_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
 
   //get single user data from github
   const getUser = async (username) => {
@@ -92,7 +82,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
