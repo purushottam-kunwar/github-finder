a39fca0ca0021d7dea67353fed88a6206aa0146a
pk.azminds <pk.azminds@gmail.com>
2020-06-26 18:12:29 +0545

    HOme And Not Found Component Added

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1> Not Found </h1>
+      <p1 className="lead"> The page you are looking for does not exist. </p1>
+    </div>
+  );
+};
+
+export default NotFound;
