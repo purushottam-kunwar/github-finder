d3055044ae941a281efa3495d2f7fb5595adc0ea
pk.azminds <pk.azminds@gmail.com>
2020-06-28 10:24:52 +0545

    page not found updated

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Image from "../pages/404_Error.jpg";
 
 const NotFound = () => {
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: "url(" + Image + ")",
+        width: "900px",
+        height: "500px",
+      }}
+    >
       <h1> Not Found </h1>
-      <p1 className="lead"> The page you are looking for does not exist. </p1>
+      {/* <p className="lead"> The page you are looking for does not exist. </p> */}
     </div>
   );
 };
